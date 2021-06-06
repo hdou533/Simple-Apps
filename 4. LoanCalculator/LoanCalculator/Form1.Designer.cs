726efb1bd616b5b38b3153f05208234387e23499
Hongnan <55491203+hdou533@users.noreply.github.com>
2021-06-06 20:56:37 +1200

    Add loan calculator app

---
+++ 4. LoanCalculator/LoanCalculator/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace LoanCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_loanAmount = new System.Windows.Forms.TextBox();
+            this.txt_numberOfMonth = new System.Windows.Forms.TextBox();
+            this.txt_interest = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_endingLoan = new System.Windows.Forms.TextBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Month:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Interest Rate:";
+            // 
+            // txt_loanAmount
+            // 
+            this.txt_loanAmount.Location = new System.Drawing.Point(239, 22);
+            this.txt_loanAmount.Name = "txt_loanAmount";
+            this.txt_loanAmount.Size = new System.Drawing.Size(189, 27);
+            this.txt_loanAmount.TabIndex = 3;
+            // 
+            // txt_numberOfMonth
+            // 
+            this.txt_numberOfMonth.Location = new System.Drawing.Point(239, 56);
+            this.txt_numberOfMonth.Name = "txt_numberOfMonth";
+            this.txt_numberOfMonth.Size = new System.Drawing.Size(189, 27);
+            this.txt_numberOfMonth.TabIndex = 4;
+            // 
+            // txt_interest
+            // 
+            this.txt_interest.Location = new System.Drawing.Point(239, 90);
+            this.txt_interest.Name = "txt_interest";
+            this.txt_interest.Size = new System.Drawing.Size(189, 27);
+            this.txt_interest.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(82, 142);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 204);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ending Balance:";
+            // 
+            // txt_endingLoan
+            // 
+            this.txt_endingLoan.Location = new System.Drawing.Point(239, 363);
+            this.txt_endingLoan.Name = "txt_endingLoan";
+            this.txt_endingLoan.Size = new System.Drawing.Size(189, 27);
+            this.txt_endingLoan.TabIndex = 8;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(82, 401);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(94, 37);
+            this.btn_calculate.TabIndex = 9;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(208, 401);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(94, 37);
+            this.btn_clear.TabIndex = 10;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(334, 401);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(94, 37);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.txt_endingLoan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txt_interest);
+            this.Controls.Add(this.txt_numberOfMonth);
+            this.Controls.Add(this.txt_loanAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Loan Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_loanAmount;
+        private System.Windows.Forms.TextBox txt_numberOfMonth;
+        private System.Windows.Forms.TextBox txt_interest;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_endingLoan;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ 4. LoanCalculator/LoanCalculator/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoanCalculator
+{
+    public partial class Form1 : Form
+    {
+
+        decimal loan_amount = 0.00m;
+        int months = 0;
+        decimal interest = 0.005m;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+
+            /*
+            try {
+                loan_amount = decimal.Parse(txt_loanAmount.Text);
+                months = int.Parse(txt_numberOfMonth.Text);
+                interest = decimal.Parse(txt_interest.Text);
+
+                int counter = 0;
+
+                for (counter = 0; counter <= months; counter++)
+                {
+                    loan_amount = loan_amount * (1 + interest);
+                    listBox1.Items.Add("At month " + counter + " you loan is " + loan_amount);
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Only numbers please.");
+            }
+
+            */
+
+
+            if ((decimal.TryParse(txt_loanAmount.Text, out loan_amount)) 
+                && (int.TryParse(txt_numberOfMonth.Text, out months)) 
+                && (decimal.TryParse(txt_interest.Text, out interest)))
+            {
+                int counter = 0;
+                while(counter <= months)
+                {
+                    loan_amount = loan_amount * (1 + interest);
+                    listBox1.Items.Add("At month " + counter + " your loan is " + loan_amount.ToString("c"));
+                    counter++;
+                }
+                txt_endingLoan.Text = loan_amount.ToString("c");
+            }
+            else
+            {
+                MessageBox.Show("Only numbers please.");
+            }
+
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_loanAmount.Text = " ";
+            txt_numberOfMonth.Text = " ";
+            txt_interest.Text = " ";
+            txt_endingLoan.Text = " ";
+            listBox1.Items.Clear();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
