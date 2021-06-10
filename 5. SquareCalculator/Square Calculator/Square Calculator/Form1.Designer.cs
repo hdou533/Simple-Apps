89b185a6b0c7e93983c9ba3eec32dde98ab115e2
Hongnan <55491203+hdou533@users.noreply.github.com>
2021-06-10 17:34:32 +1200

    Add a square calculator

---
+++ 5. SquareCalculator/Square Calculator/Square Calculator/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Square_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_start = new System.Windows.Forms.TextBox();
+            this.txt_stop = new System.Windows.Forms.TextBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start at";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stop at";
+            // 
+            // txt_start
+            // 
+            this.txt_start.Location = new System.Drawing.Point(89, 21);
+            this.txt_start.Name = "txt_start";
+            this.txt_start.Size = new System.Drawing.Size(125, 27);
+            this.txt_start.TabIndex = 2;
+            // 
+            // txt_stop
+            // 
+            this.txt_stop.Location = new System.Drawing.Point(89, 55);
+            this.txt_stop.Name = "txt_stop";
+            this.txt_stop.Size = new System.Drawing.Size(125, 27);
+            this.txt_stop.TabIndex = 3;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(228, 14);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(83, 33);
+            this.btn_calculate.TabIndex = 4;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(17, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(305, 424);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 528);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.txt_stop);
+            this.Controls.Add(this.txt_start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Square Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_start;
+        private System.Windows.Forms.TextBox txt_stop;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 5. SquareCalculator/Square Calculator/Square Calculator/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Square_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            int startNum = 0;
+            int stopNum = 10;
+
+            startNum = int.Parse(txt_start.Text);
+            stopNum = int.Parse(txt_stop.Text);
+
+            int x = 0;
+
+            for(x=startNum; x<=stopNum; x++)
+            {
+                int square_value = x * x;
+                listBox1.Items.Add("The squared value of " + x + " is " + square_value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_start.Text = " ";
+            txt_stop.Text = " ";
+            listBox1.Items.Clear();
+        }
+    }
+}
